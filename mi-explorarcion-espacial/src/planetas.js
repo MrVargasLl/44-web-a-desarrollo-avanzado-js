33460a2aa3875e417a56eeb11821cf4945b7d529
Alvaro Enrique Vargas Llanos <alvaro.vargas.mail@gmail.com>
2026-02-16 23:31:37 -0400

    Proyecto de Intro a Node.js y npm

---
+++ mi-explorarcion-espacial/src/planetas.js
@@ -0,0 +1,34 @@
+export const planetas = [
+    {
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655"
+    },
+    {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016"
+    },
+    {
+    nombre: "Marte",
+    descripcion: "El planeta rojo, conocido por su superficie rocosa y posibles rastros de agua pasada.",
+    descubiertoEn: "Conocido desde la antigüedad"
+    },
+    {
+    nombre: "Júpiter",
+    descripcion: "El planeta más grande del sistema solar, famoso por su Gran Mancha Roja.",
+    descubiertoEn: "Conocido desde la antigüedad"
+    },
+    {
+    nombre: "Neptuno",
+    descripcion: "Un gigante gaseoso azul con los vientos más rápidos del sistema solar.",
+    descubiertoEn: "1846"
+    },
+    {
+    nombre: "Kepler-452b",
+    descripcion: "Un exoplaneta considerado un posible primo de la Tierra por su tamaño y órbita.",
+    descubiertoEn: "2015"
+    }
+  // ¡Añade más planetas!
+];
+/* module.exports = planetas; */
